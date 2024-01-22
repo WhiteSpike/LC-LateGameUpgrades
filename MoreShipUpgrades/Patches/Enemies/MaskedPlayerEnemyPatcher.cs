909e0de3ca027d24702862e6d51b4e39084e353b
WhiteSpike <58040140+WhiteSpike@users.noreply.github.com>
2024-01-22 16:38:40 +0000

    Create MaskedPlayerEnemyPatcher.cs

---
+++ MoreShipUpgrades/Patches/Enemies/MaskedPlayerEnemyPatcher.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using MoreShipUpgrades.Misc;
+using MoreShipUpgrades.UpgradeComponents.Items.BarbedWire;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MoreShipUpgrades.Patches.Enemies
+{
+    [HarmonyPatch(typeof(MaskedPlayerEnemy))]
+    internal class MaskedPlayerEnemyPatcher
+    {
+        const float PATROL_RUNNING_SPEED = 7f;
+        const float WALKING_SPEED = 3.8f;
+        const float CHASE_RUNNING_SPEED = 8f;
+        const float TOWARDS_SHIP_SPEED = 5f;
+        [HarmonyPatch(nameof(MaskedPlayerEnemy.Update))]
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> UpdateTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            int index = 0;
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
+            index = PatchAgentRunningSpeedWhenPatrolling(index, ref codes);
+            index = PatchAgentWalkingSpeedWhenPatrolling(index, ref codes);
+            index = PatchAgentRunningSpeedWhenChasing(index, ref codes);
+            index = PatchAgentWalkingSpeedWhenPatrolling(index, ref codes);
+            index = PatchAgentWalkingSpeedWhenHidingInShip(index, ref codes);
+            return codes;
+        }
+        static int PatchAgentWalkingSpeedWhenHidingInShip(int index, ref List<CodeInstruction> codes)
+        {
+            MethodInfo checkForBarbedWires = typeof(BaseBarbedWire).GetMethod(nameof(BaseBarbedWire.CheckForBarbedWires));
+            return Tools.LookForFloat(index, ref codes, TOWARDS_SHIP_SPEED, checkForBarbedWires, true, "Couldn't find agent running speed when patrolling");
+        }
+        static int PatchAgentRunningSpeedWhenChasing(int index, ref List<CodeInstruction> codes)
+        {
+            MethodInfo checkForBarbedWires = typeof(BaseBarbedWire).GetMethod(nameof(BaseBarbedWire.CheckForBarbedWires));
+            return Tools.LookForFloat(index, ref codes, CHASE_RUNNING_SPEED, checkForBarbedWires, true, "Couldn't find agent running speed when patrolling");
+        }
+        static int PatchAgentWalkingSpeedWhenPatrolling(int index, ref List<CodeInstruction> codes)
+        {
+            MethodInfo checkForBarbedWires = typeof(BaseBarbedWire).GetMethod(nameof(BaseBarbedWire.CheckForBarbedWires));
+            return Tools.LookForFloat(index, ref codes, WALKING_SPEED, checkForBarbedWires, true, "Couldn't find agent running speed when patrolling");
+        }
+
+        static int PatchAgentRunningSpeedWhenPatrolling(int index, ref List<CodeInstruction> codes)
+        {
+            MethodInfo checkForBarbedWires = typeof(BaseBarbedWire).GetMethod(nameof(BaseBarbedWire.CheckForBarbedWires));
+            return Tools.LookForFloat(index, ref codes, PATROL_RUNNING_SPEED, checkForBarbedWires, true, "Couldn't find agent running speed when patrolling");
+        }
+    }
+}
