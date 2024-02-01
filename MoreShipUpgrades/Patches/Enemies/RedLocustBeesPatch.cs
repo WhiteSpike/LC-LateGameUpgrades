b1925fb94e40773731a9dc823dbb2d2ef56d1ee7
WhiteSpike <58040140+WhiteSpike@users.noreply.github.com>
2024-02-01 20:14:12 +0000

    Changing patch method to be update with Tools' implementation

--- MoreShipUpgrades/Patches/Enemies/RedLocustBeesPatch.cs
+++ MoreShipUpgrades/Patches/Enemies/RedLocustBeesPatch.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using GameNetcodeStuff;
+using HarmonyLib;
 using MoreShipUpgrades.Misc;
 using MoreShipUpgrades.UpgradeComponents.TierUpgrades;
 using System;
@@ -19,29 +20,10 @@ namespace MoreShipUpgrades.Patches.Enemies
         public static IEnumerable<CodeInstruction> OnCollideWithPlayer_Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             MethodInfo beeReduceDamage = typeof(beekeeperScript).GetMethod(nameof(beekeeperScript.CalculateBeeDamage));
+            MethodInfo damagePlayer = typeof(PlayerControllerB).GetMethod(nameof(PlayerControllerB.DamagePlayer));
             List<CodeInstruction> codes = instructions.ToList();
-            bool found = false;
-            for (int i = 0; i < codes.Count; i++)
-            {
-                if (found) break;
-                if (!(codes[i].opcode == OpCodes.Callvirt && codes[i].operand.ToString() == "Void DamagePlayer(Int32, Boolean, Boolean, CauseOfDeath, Int32, Boolean, UnityEngine.Vector3)")) continue;
-
-                /* 
-                * ldc.i4.s  10  -> damageNumber
-                * ldc.i4.1      -> damageSFX
-                * ldc.i4.1      -> callRPC
-                * ldc.i4.s  11  -> CauseDeath
-                * ldc.i4.3      -> DeathAnimationTime
-                * ldc.i4.0      -> fallDamage
-                * ldloca.s V_1  
-                * initobj[UnityEngine.CoreModule]UnityEngine.Vector3
-                * ldloc.1       -> force
-                * callvirt instance void GameNetcodeStuff.PlayerControllerB::DamagePlayer(int32, bool, bool, valuetype CauseOfDeath, int32, bool, valuetype[UnityEngine.CoreModule]UnityEngine.Vector3)
-                */
-                codes.Insert(i - 8, new CodeInstruction(OpCodes.Call, beeReduceDamage));
-                found = true;
-            }
-            if (!found) { logger.LogError("Did not find DamagePlayer function"); }
+            int index = 0;
+            index = Tools.FindInteger(index, ref codes, findValue: 10, addCode: beeReduceDamage, errorMessage: "Couldn't find damage number for player");
             return codes.AsEnumerable();
         }
 
